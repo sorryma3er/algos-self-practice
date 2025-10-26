0f44cb60ea2a652ea61ed9b6e8282efe54bc3caf
sorryma3er <whc0713air@gmail.com>
2025-10-26 14:51:27 +0700

    Added solution to Longest Common Prefix in .java version.

---
+++ Problems/Longest_Common_Prefix/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package Longest_Common_Prefix;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.longestCommonPrefix(new String[] {"aaa", "aa", "aaa"}));
+    }
+}
+
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String target = strs[0];
+        int idx = 1;
+
+        while (target.length() > 0 && idx < strs.length){
+            String toComapre = strs[idx];
+            if (toComapre.length() == 0) return "";
+            for (int j = 0; j < toComapre.length() && j < target.length(); j++){
+                if (target.charAt(j) != toComapre.charAt(j)) {
+                    target = target.substring(0, j);
+                    System.out.println(target);
+                    break;
+                }
+                else if (target.charAt(j) == toComapre.charAt(j) && j == toComapre.length() - 1){
+                    target = target.substring(0, j + 1);
+                }
+            }
+            idx++;
+        }
+
+        return target;
+    }
+}

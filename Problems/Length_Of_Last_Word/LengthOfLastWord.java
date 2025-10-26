fc4662d4217abcfbc9f4ffd7c27e523348da966a
sorryma3er <whc0713air@gmail.com>
2025-10-26 14:15:50 +0700

    Added Length Of Last Word solution in .java and c# versions.

---
+++ Problems/Length_Of_Last_Word/LengthOfLastWord.java
@@ -0,0 +1,15 @@
+package Length_Of_Last_Word;
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String[] words = s.trim().split(" ");
+        return words.length == 0 ? 0 : words[words.length - 1].length();
+    }
+}
+
+public class LengthOfLastWord{
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLastWord("   fly me   to   the moon  "));
+    }
+}
+
---
+++ Problems/Length_Of_Last_Word/LengthOfLastWordApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries); // remove the "" empty entry
+        return words.Length == 0 ? 0 : words[^1].Length;
+    }
+
+    public static void Main(string[] args)
+    {
+        string str = "   fly me   to   the moon  ";
+        int sth = new Solution().LengthOfLastWord(str);
+        Console.WriteLine(sth);
+    }
+}
+

124f2818a33fb277d86f44b1ea2413c6b8298c6a
sorryma3er <whc0713air@gmail.com>
2025-11-12 22:36:06 +0700

    Added solution to Is SubSequence in .py version without using two pointer thought.

---
+++ Problems/TwoPointersType/Is_SubSequence/is_subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    # didnt make use of two pointers thought tho
+    def isSubsequence(self, s: str, t: str) -> bool:
+        compare_idx: int = 0
+        for i in range(len(t)):
+            if compare_idx == len(s): return True
+            if t[i] == s[compare_idx]:
+                compare_idx += 1
+
+        return True if compare_idx == len(s) else False   
+   
+if __name__ == "__main__":
+    test: dict[tuple[str, str], bool] = {("abc","ahbgdc"): True, ("axc","ahbgdc"): False} 
+    # use dict.items() extract all items in the dict
+    for (key, val), flag in test.items():
+        got: bool = Solution().isSubsequence(key, val)
+        print(f"Got: {got} and expected: {flag}")
